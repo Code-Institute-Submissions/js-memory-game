cd94422f83865319f5a71d760ae9e13b391c4027
Tipoki13 <conorhumphreys1@gmail.com>
2020-04-08 16:08:19 +0000

    CSS updating being done for hiding icons in Li elements. Also, jQuery updating done for click function to show icons once clicked by the user

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 /*----Displaying Cards----*/
 
-$(".card").click(function() {
-  $(".card").show("medium");
-});
-$(".card").click(function() {
-  $(".card").hide("medium");
-});
+// Click on card to display icon
+
+$('li').click(function (){
+    $(this).children().show();
+})
+
+// Install shuffle function on page load and start game
+
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+
+const deck = document.querySelector(".deck");
+function startGame(){
+   var shuffledCards = shuffle(cards);
+   for (var i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards, function(item){
+         deck.appendChild(item);
+      });
+   }
+}
+
+window.onload = startGame();
