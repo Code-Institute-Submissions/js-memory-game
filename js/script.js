3bd8d190b3904815bf104864bff3773334e47c47
Tipoki13 <conorhumphreys1@gmail.com>
2020-04-15 19:51:46 +0000

    Cards match/unmatch if statement updated

--- js/script.js
+++ js/script.js
@@ -3,18 +3,35 @@
 // var card1;
 var the_cards = [];
 // var card2;
-var card2 = [];
+var match = 0;
+
 
 // Click on card to display icon and add to relevant array
 
 $('li').click(function (){
     $(this).children().show();
+    $(this).addClass('clicked');
     console.log(this.type);
-    the_cards.push(this.type);
-    console.log(the_cards);
-    if (the_cards.length == 2) {
-        if (the_cards[0] == the_cards[1]) {
+    the_cards.push(this);
 
+    console.log(the_cards);
+    console.log("Test the_cards.length == 2 : ",  the_cards.length == 2);
+    if (the_cards.length == 2) {
+        console.log("We are in the if statement now");
+        var card_1 = the_cards[0];
+        var card_2 = the_cards[1];
+        console.log("Test card_1.type == card_2.type :", card_1.type == card_2.type)
+        if (card_1.type == card_2.type) {
+            // console.log("Test $('li').children().addClass('match') :", $('li').children().addClass('match'))
+            $('.clicked').addClass('match');
+            match++;
+        } else {
+            $('.clicked').addClass('notmatch');
+            setTimeout (function() {
+                $('.notmatch').hide();
+            }, 2000)
+            the_cards = [];
+            console.log(the_cards);
         }
     }
     
@@ -40,6 +57,7 @@ $('li').click(function (){
 
 let card = document.getElementsByClassName("card");
 let cards = [...card];
+console.log('cards',cards);
 
 // Employ Fisher-Yates shuffle
 
@@ -60,11 +78,12 @@ const deck = document.querySelector(".deck");
 function startGame(){
     // Shuffle cards
    let shuffledCards = shuffle(cards);
-   for (var i= 0; i < shuffledCards.length; i++){
-      [].forEach.call(shuffledCards, function(item){
-         deck.appendChild(item);
-      });
-   }
+   console.log('shuffledCards', shuffledCards);
+//    for (var i= 0; i < shuffledCards.length; i++){
+//       [].forEach.call(shuffledCards, function(item){
+//          deck.appendChild(item);
+//       });
+//    }
 }
 
 window.onload = startGame();
